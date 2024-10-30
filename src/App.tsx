74d687b86b0101018dd5a1e4d9e3ac0ba61117f3
Md Rafidul Islam <rafidulemon@gmail.com>
2024-10-30 11:21:47 +0600

    initial

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen w-full bg-blue-500 text-white text-2xl">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
