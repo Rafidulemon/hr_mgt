be6b81214e83e97f37b14149135e44847f379b25
Md Rafidul Islam <rafidulemon@gmail.com>
2024-11-02 09:11:21 +0600

    atoms page updated

--- src/pages/atoms/index.tsx
+++ src/pages/atoms/index.tsx
@@ -4,26 +4,28 @@ import { Card } from "../../components/atoms/frame/Card";
 function Atoms() {
   return (
     <div className="flex flex-col w-full h-screen p-10 bg-[#ECECEC]">
-      <div className="flex flex-row w-full justify-center items-center text-[24px] font-extrabold">
-        <h1 className="text-center">Components</h1>
+      <div className="flex flex-row w-full justify-center items-center text-[30px] font-extrabold">
+        <h1 className="text-center mb-6">Components</h1>
       </div>
-      <Card className="h-[300px] p-6 grid grid-cols-2 gap-6">
-        <div className="col-span-1 flex flex-col gap-4 text-black text-[24px]">
+      <Card className="h-[300px] p-6 gap-2 grid grid-cols-2">
+        <span className="col-span-2 text-[24px] font-bold">Buttons</span>
+        <line className="col-span-2 w-full border-b border-black mb-6"/>
+        <div className="col-span-1 flex flex-col gap-1 text-black text-[18px]">
           <h3>Primary Button:</h3>
           <Button theme="primary" className="w-[250px] text-[16px]">Primary Button</Button>
         </div>
 
-        <div className="col-span-1 flex flex-col gap-4 text-black text-[24px]">
+        <div className="col-span-1 flex flex-col gap-1 text-black text-[18px]">
           <h3>Secondary Button:</h3>
           <Button theme="secondary" className="w-[250px] text-[16px]">Secondary Button</Button>
         </div>
 
-        <div className="flex flex-col gap-4 text-black text-[24px]">
+        <div className="flex flex-col gap-1 text-black text-[18px]">
           <h3>Cancel Button:</h3>
           <Button theme="cancel" className="w-[250px] text-[16px]">Cancel Button</Button>
         </div>
 
-        <div className="flex flex-col gap-4 text-black text-[24px]">
+        <div className="flex flex-col gap-1 text-black text-[18px]">
           <h3>Cancel Secondary Button:</h3>
           <Button theme="cancel-secondary" className="w-[250px] text-[16px]">Cancel Secondary Button</Button>
         </div>
