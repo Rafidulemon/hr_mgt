eb63b985ebfb4df0bc9aedd11a3293f56983c41c
Shariar Duke <shariarmahmud225@gmail.com>
2024-11-02 17:41:52 +0600

    text component

--- src/components/atoms/Text/Text.tsx
+++ src/components/atoms/Text/Text.tsx
@@ -1,3 +1,37 @@
-export default function Text() {
-  return <div>This will be the Text Component</div>;
-}
+import React from "react";
+
+type Props = {
+  className?: string;
+  text: string;
+  isBold?: boolean;
+  color?: string;
+  align?: "left" | "center" | "right";
+  lineHeight?: string;
+  fontFamily?: string;
+};
+
+const Text: React.FC<Props> = ({
+  className = "",
+  text,
+  isBold = false,
+  color,
+  align = "left",
+  lineHeight,
+  fontFamily,
+}) => {
+  return (
+    <p
+      className={`${className} ${align}`}
+      style={{
+        fontWeight: isBold ? 700 : 400,
+        color,
+        lineHeight,
+        fontFamily,
+      }}
+    >
+      {text}
+    </p>
+  );
+};
+
+export default Text;
