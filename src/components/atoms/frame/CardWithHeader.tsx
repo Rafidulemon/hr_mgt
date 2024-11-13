14d2ecb50f71fee1c12250491a9f01ed2692ef4a
Md Rafidul Islam <rafidulemon@gmail.com>
2024-11-13 12:30:06 +0600

    pull and push

--- src/components/atoms/frame/CardWithHeader.tsx
+++ src/components/atoms/frame/CardWithHeader.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Text from "../Text/Text";
 
 type Props = {
   className?: string;
@@ -11,15 +12,15 @@ type Props = {
 >;
 
 export const CardWithHeader = (props: Props) => {
-  const { className, title, titleColor = "", titleBarSize = "40px", ...others } = props;
+  const { className, title="", titleColor = "", titleBarSize = "40px", ...others } = props;
   return (
     <div className={`w-full h-full flex flex-col gap-0 ${className}`}>
         <div className={`relative z-10 ${titleBarSize} ${titleColor ? titleColor : "bg-[#008EA1]"} rounded-t-3xl shadow-md p-4 flex flex-row items-center justify-center`}>
-            <h2 className={`text-[16px] text-white font-extrabold ${titleColor}`}>{title}</h2>
+            <Text text={title} className={`text-[16px] text-white font-extrabold ${titleColor}`} isBold/>
         </div>
       <div
         {...others}
-        className={`-mt-[40px] h-full w-full bg-white rounded-3xl shadow`}
+        className={`h-full w-full bg-white rounded-b-3xl shadow`}
       />
     </div>
   );
