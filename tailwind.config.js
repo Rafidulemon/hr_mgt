202bb94d1f454d66e906826d8306d934544a14da
Shariar Duke <shariarmahmud225@gmail.com>
2024-11-14 12:24:10 +0600

    global color fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#0DBAD2",
-        text_primary: "#7E7D7D",
-        text_bold: "#555454",
-        tertiary: "#D20D0D"
-      }
+        primary: "var(--primary-color)",
+        text_primary: "var(--text_primary-color)",
+        text_bold: "var(--text_bold-color)",
+        tertiary: "var(--tertiary-color)",
+      },
     },
   },
   plugins: [],
-}
+};
