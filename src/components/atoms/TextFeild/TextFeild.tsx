2378a284d3fa5c1ac4191ff31fb1df839228e0b8
Shariar Duke <shariarmahmud225@gmail.com>
2024-11-14 10:15:08 +0600

    TextFeild component completed

--- src/components/atoms/TextFeild/TextFeild.tsx
+++ src/components/atoms/TextFeild/TextFeild.tsx
@@ -3,28 +3,52 @@ type Props = {
   text: string;
   labelColor?: string;
   textColor?: string;
+  labelFontFamily?: string;
+  textFontFamily?: string;
+  labelFontWeight?: number;
   labelFontSize?: string;
+  textFontWeight?: number;
   textFontSize?: string;
-  textFontWeight?: string;
-  textFontSize?: string;
-  gap?: string;
+  className?: string;
 };
-export default function TextFeild(props: Props) {
+const TextFeild = (props: Props) => {
   const {
     label,
     text,
     labelColor = "#555454",
-    labelFontWeight = "bold",
-    labelFontSize = "16px",
     textColor = "#7E7D7D",
-    textFontWeight = "normal",
+    labelFontFamily = "Inter, sans-serif",
+    textFontFamily = "Inter, sans-serif",
+    labelFontWeight = 600,
+    labelFontSize = "16px",
+    textFontWeight = 600,
     textFontSize = "14px",
-    gap = "20px",
+    className,
   } = props;
   return (
-    <div className={`flex flex-col gap-${gap}`}>
-      <p>{label}</p>
-      <p>{text}</p>
+    <div className={`flex flex-col gap-[20px] ${className}`}>
+      <p
+        style={{
+          color: labelColor,
+          fontWeight: labelFontWeight,
+          fontSize: labelFontSize,
+          fontFamily: labelFontFamily,
+        }}
+      >
+        {label}
+      </p>
+      <p
+        style={{
+          color: textColor,
+          fontWeight: textFontWeight,
+          fontSize: textFontSize,
+          fontFamily: textFontFamily,
+        }}
+      >
+        {text}
+      </p>
     </div>
   );
-}
+};
+
+export default TextFeild;
