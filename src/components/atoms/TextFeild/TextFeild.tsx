16cb0e222813035f419994a6680459859802c42f
Shariar Duke <shariarmahmud225@gmail.com>
2024-11-14 09:20:45 +0600

    initial

--- src/components/atoms/TextFeild/TextFeild.tsx
+++ src/components/atoms/TextFeild/TextFeild.tsx
@@ -1,7 +1,30 @@
-export default function TextFeild() {
+type Props = {
+  label: string;
+  text: string;
+  labelColor?: string;
+  textColor?: string;
+  labelFontSize?: string;
+  textFontSize?: string;
+  textFontWeight?: string;
+  textFontSize?: string;
+  gap?: string;
+};
+export default function TextFeild(props: Props) {
+  const {
+    label,
+    text,
+    labelColor = "#555454",
+    labelFontWeight = "bold",
+    labelFontSize = "16px",
+    textColor = "#7E7D7D",
+    textFontWeight = "normal",
+    textFontSize = "14px",
+    gap = "20px",
+  } = props;
   return (
-    <div>
-      <p>Text Feild</p>
+    <div className={`flex flex-col gap-${gap}`}>
+      <p>{label}</p>
+      <p>{text}</p>
     </div>
   );
 }
