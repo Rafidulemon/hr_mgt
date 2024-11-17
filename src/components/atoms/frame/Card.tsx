1838d87ba727da178777c7b3fdc2f9164bdc28ef
Md Rafidul Islam <rafidulemon@gmail.com>
2024-11-17 15:46:52 +0600

    Card fixed

--- src/components/atoms/frame/Card.tsx
+++ src/components/atoms/frame/Card.tsx
@@ -1,16 +1,37 @@
 import React from "react";
+import Text from "../Text/Text";
 
-type Props = { className?: string, background?: string, isSquareBox?: boolean; } & React.DetailedHTMLProps<
+type Props = {
+  className?: string;
+  background?: string;
+  isSquareBox?: boolean;
+  title?: string;
+  isTransparentBackground?: boolean;
+} & React.DetailedHTMLProps<
   React.HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
 >;
 
 export const Card = (props: Props) => {
-  const { className,background,isSquareBox = false, ...others } = props;
+  const {
+    className,
+    background,
+    isSquareBox = false,
+    isTransparentBackground = false,
+    title,
+    ...others
+  } = props;
   return (
     <div
-      {...others}
-      className={`h-full w-full ${isSquareBox ? "": "rounded-3xl"} shadow ${className} ${background ? `bg-${background}` : "bg-white"}`}
-    />
+      className={`h-full w-full py-4 ${
+        isSquareBox ? "" : "rounded-3xl"
+      } ${!isTransparentBackground && "shadow"} ${className} ${isTransparentBackground && "bg-transparent"} ${background ? `bg-${background}` : "bg-white"}`}
+    >
+      {title && (
+        <Text text={title} className="px-6 text-[24px] font-semibold" />
+      )}
+      <hr className={`w-full border border-[1px] border-[#7E7D7D] my-4 ${isTransparentBackground && "mx-6"}`} />
+      <div {...others} className="px-6" />
+    </div>
   );
 };
