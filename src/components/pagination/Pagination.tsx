c087b18091d237a13e14c68549fedc3a6fdb1f63
Shariar Duke <shariarmahmud225@gmail.com>
2024-11-27 11:14:44 +0600

    pagination complete

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,64 +1,50 @@
-import { useState } from "react";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import Button from "../atoms/buttons/Button";
-import Text from "../atoms/Text/Text";
+import { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 
-export default function Pagination() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = 6;
-  const pageArray = [1, 2, 3, 4, 5, 6, 7, 8];
+type PaginationProps<T> = {
+  data: T[];
+  postsPerPage: number;
+  setCurrentPageData: (currentPageData: T[]) => void;
+};
 
-  const handlePrevious = () => {
-    if (currentPage > 1) setCurrentPage(currentPage - 1);
+export default function Pagination<T>({
+  data,
+  postsPerPage,
+  setCurrentPageData,
+}: PaginationProps<T>) {
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const pageCount = Math.ceil(data.length / postsPerPage);
+
+  const changePage = ({ selected }: { selected: number }) => {
+    setPageNumber(selected);
   };
 
-  const handleNext = () => {
-    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
-  };
+  useEffect(() => {
+    const startIndex = pageNumber * postsPerPage;
+    const currentPageData = data.slice(startIndex, startIndex + postsPerPage);
+    setCurrentPageData(currentPageData);
+  }, [pageNumber, data, postsPerPage, setCurrentPageData]);
 
   return (
-    <div className="flex justify-center items-center space-x-2 mt-4">
-      <div className="bg-white shadow-lg  h-[50px] overflow-hidden max-w-[400px] flex items-center space-x-2">
-        <Button
-          onClick={handlePrevious}
-          isDisabled={currentPage === 1}
-          theme="white"
-        >
-          <FaChevronLeft className="text-gray-700" />
-        </Button>
-        <div className="flex gap-[16px]">
-          {pageArray.map((page) => (
-            <div key={page}>
-              {page % 4 === 0 ? (
-                <div
-                  onClick={() => setCurrentPage(page)}
-                  className={`px-[8px] py-[6px] hover:bg-primary hover:text-white ${
-                    page === currentPage
-                      ? "bg-primary text-white cursor-pointer"
-                      : "text-[#555454]"
-                  }`}
-                >
-                  <Text
-                    className="text-[12px] cursor-pointer"
-                    text={`${page}`}
-                  />
-                </div>
-              ) : (
-                <div className="px-[8px] py-[6px]">
-                  <Text className="text-[12px] cursor-pointer" text="..." />
-                </div>
-              )}
-            </div>
-          ))}
+    <div className="mt-4">
+      <div className="flex justify-end items-center">
+        <div className="pb-4 px-4 bg-white shadow-xl">
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel=">"
+            onPageChange={changePage}
+            pageRangeDisplayed={5}
+            pageCount={pageCount}
+            previousLabel="<"
+            containerClassName="flex justify-center items-center gap-2 mt-4"
+            pageClassName="px-2 py-1 border border-gray-400 rounded"
+            activeClassName="bg-primary text-white"
+            previousClassName="px-2 py-1 border border-gray-400 rounded"
+            nextClassName="px-2 py-1 border border-gray-400 rounded"
+            disabledClassName="opacity-50 cursor-not-allowed bg-gray-400 text-black font-extrabold"
+          />
         </div>
-
-        <Button
-          onClick={handleNext}
-          isDisabled={currentPage === totalPages}
-          theme="white"
-        >
-          <FaChevronRight className="text-gray-700" />
-        </Button>
       </div>
     </div>
   );
--- src/pages/attendance/history.tsx
+++ src/pages/attendance/history.tsx
@@ -2,301 +2,312 @@ import { useState } from "react";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import Table from "../../components/atoms/tables/Table";
 import Text from "../../components/atoms/Text/Text";
+import Pagination from "../../components/pagination/Pagination";
 import { months } from "../../utils/dateAndMonth";
+const attendenceTableHeader = [
+  "Date",
+  "Day of the week",
+  "Start Time",
+  "End Time",
+  "Start Time2",
+  "End Time2",
+  "Working Hours",
+];
+
+interface Row {
+  Date: string;
+  "Day of the week": string;
+  "Start Time": string;
+  "End Time": string;
+  "Start Time2": string;
+  "End Time2": string;
+  "Working Hours": string;
+}
+
+const rows: Row[] = [
+  {
+    Date: "01/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "02/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "03/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "04/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "05/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "06/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "07/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "08/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "09/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "10/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "11/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "12/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "13/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "14/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "15/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "16/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "17/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+
+  {
+    Date: "18/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "19/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+
+  {
+    Date: "20/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "21/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+
+  {
+    Date: "22/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "23/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "24/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "25/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "26/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+
+  {
+    Date: "27/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+  {
+    Date: "28/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+
+  {
+    Date: "29/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+
+  {
+    Date: "30/11",
+    "Day of the week": "Friday",
+    "Start Time": "08:30:00",
+    "End Time": "12:30:00",
+    "Start Time2": "13:30:00",
+    "End Time2": "17:30:00",
+    "Working Hours": "8:00",
+  },
+];
 
 export default function AttendanceHistory() {
   const date = new Date();
   const currentMonthIndex = date.getMonth();
   const [currentMonth, setCurrentMonth] = useState<number>(currentMonthIndex);
-
-  const attendenceTableHeader = [
-    "Date",
-    "Day of the week",
-    "Start Time",
-    "End Time",
-    "Start Time2",
-    "End Time2",
-    "Working Hours",
-  ];
-
-  const rows = [
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-
-    {
-      Date: "01/11",
-      "Day of the week": "Friday",
-      "Start Time": "08:30:00",
-      "End Time": "12:30:00",
-      "Start Time2": "13:30:00",
-      "End Time2": "17:30:00",
-      "Working Hours": "8:00",
-    },
-  ];
+  const [currentPageData, setCurrentPageData] = useState<Row[]>();
 
   const preMonth = () => {
     setCurrentMonth((prev) => (prev === 0 ? 11 : prev - 1));
@@ -331,25 +342,22 @@ export default function AttendanceHistory() {
       </div>
 
       <div className="mt-[20px]">
-        <Table
-          headers={attendenceTableHeader}
-          rows={rows}
-          className="shadow-lg py-6"
-          isTextCenter
-        />
+        {currentPageData && (
+          <Table
+            headers={attendenceTableHeader}
+            rows={currentPageData}
+            className="shadow-lg py-6"
+            isTextCenter
+          />
+        )}
       </div>
 
-      <div className="flex justify-start items-center mt-8 ">
-        <div className="flex  gap-[60px]">
-          <Text
-            text="Total Working Hour"
-            className="text-[24px] font-semibold"
-          />
-          <Text
-            text="160:00"
-            className="text-[24px] font-semibold text-[#555454]"
-          />
-        </div>
+      <div>
+        <Pagination
+          data={rows}
+          postsPerPage={10}
+          setCurrentPageData={setCurrentPageData}
+        />
       </div>
     </div>
   );
