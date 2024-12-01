50f5a88a23e30963155d6898e841f14ed315a3cd
Shariar Duke <shariarmahmud225@gmail.com>
2024-12-01 14:56:58 +0600

    initial design done

--- src/pages/report/monthly/index.tsx
+++ src/pages/report/monthly/index.tsx
@@ -1,3 +1,115 @@
+import { EmployeeHeader } from "../../../components/layouts/EmployeeHeader";
+import { useNavigate } from "react-router-dom";
+import Text from "../../../components/atoms/Text/Text";
+import { useState } from "react";
+import Button from "../../../components/atoms/buttons/Button";
+import { MdDeleteForever } from "react-icons/md";
+import TextInput from "../../../components/atoms/inputs/TextInput";
 export default function MonthlyReportApplication() {
-  return <div>Duke</div>;
+  const navigate = useNavigate();
+  const day = new Date();
+  const today = new Intl.DateTimeFormat("en-us", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  }).format(day);
+  const [reports, setReports] = useState([
+    {
+      id: 1,
+      taskName: "",
+      storyPoint: "",
+      workingHours: "",
+    },
+  ]);
+
+  const handleAddReport = () => {
+    const newReport = {
+      id: reports.length + 1,
+      taskName: "",
+      storyPoint: "",
+      workingHours: "",
+    };
+
+    setReports((prev) => {
+      return [...prev, newReport];
+    });
+  };
+
+  const handleDeleteReport = (id: number) => {
+    setReports((prevReports) =>
+      prevReports.filter((report) => report.id != id)
+    );
+  };
+
+  console.log("Now the reports are", reports);
+  return (
+    <div className="w-full flex flex-col gap-10">
+      <EmployeeHeader
+        name="Md. Rafidul Islam"
+        designation="Software Engineer"
+        joining_date="Aug 17, 2023"
+        hasRightButton
+        buttonText="History"
+        onButtonClick={() => navigate("/monthly/report-history")}
+      />
+      <div className="w-full bg-white shadow p-8 flex flex-col gap-6">
+        <div className="flex flex-col gap-2 mb-6">
+          <Text text="Daily Report" className="text-[24px] font-semibold" />
+          <Text text={today} className="text-text_primary" />
+        </div>
+
+        {reports.map((report, index) => (
+          <div className="border-b pb-6 mb-10" key={report.id}>
+            <div className="col-span-2 flex justify-end">
+              <button
+                onClick={() => handleDeleteReport(report?.id)}
+                className="text-[30px] hover:text-red-500 mb-4"
+                aria-label={`Delete Report ${index + 1}`}
+              >
+                <MdDeleteForever />
+              </button>
+            </div>
+            <div className="grid grid-cols-2 gap-10">
+              <TextInput
+                label={`Task Name / Ticket Number ${report.id}`}
+                placeholder="Enter Task Name"
+                isRequired
+              />
+              <TextInput
+                placeholder="Enter Story point"
+                label={`Story Point ${report.id}`}
+                isRequired
+              />
+              <TextInput
+                placeholder="Enter Working Hours"
+                label={`Working Hours ${report.id}`}
+                isRequired
+              />
+            </div>
+          </div>
+        ))}
+
+        <Button
+          theme="secondary"
+          className="w-[185px]"
+          onClick={handleAddReport}
+        >
+          <Text text="Add New Report" className="font-semibold" />
+        </Button>
+
+        <div className="w-full flex flex-row gap-6 mt-10">
+          <Button type="submit" className="w-[185px]">
+            <Text text="Submit" className="font-semibold" />
+          </Button>
+          <Button
+            theme="cancel"
+            className="w-[185px]"
+            onClick={() => navigate("/monthly/report-history")}
+          >
+            <Text text="Cancel" className="font-semibold" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
