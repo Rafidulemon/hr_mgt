3af0f8423ed5013e5b0a0cc1a8bca0377621615e
Shariar Duke <shariarmahmud225@gmail.com>
2024-12-16 08:04:21 +0600

    timestamp adde in the table

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,12 +1,14 @@
-
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text  # Fix typo here
 from .database import Base  # Ensure this is correct
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    address = Column(String)
-    role = Column(String)
-    is_active = Column(Integer)
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+    role = Column(String, nullable=False)
+    is_active = Column(Integer, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))  # Timestamp default
